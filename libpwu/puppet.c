ba5378d53583157e95e47510b6510937ef29ddf3
vykt <knikanchyk@icloud.com>
2023-05-20 05:27:29 +0100

    test new pattern.c

--- libpwu/puppet.c
+++ libpwu/puppet.c
@@ -121,7 +121,7 @@ int change_region_perms(puppet_info * p_info, byte perms, int fd_mem,
 		if (ret == 0) continue;
 
 		//if a match was found, carry out mprotect syscall
-		ret = vector_get(&ptn.instance_vector, 0, (byte *) &syscall_offset);
+		ret = vector_get(&ptn.offset_vector, 0, (byte *) &syscall_offset);
 		if (ret == -1) return -1;
         syscall_addr = m_entry_ref->start_addr + syscall_offset;
 		ret = puppet_save_regs(p_info);
